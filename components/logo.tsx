452a3c51f0495b8d8f1ccb0d047e573127c6bc2a
Alami ERHAIDI <alamierhaidi@iMac-de-Alami-2.local>
2026-04-28 16:20:39 +0200

    first commit

--- components/logo.tsx
+++ components/logo.tsx
@@ -10,22 +10,22 @@ export const Logo = () => (
     <div className="flex-shrink-0">
       <Image 
         src="/images/logo400.jpg" // Note: j'ai gardé votre faute de frappe "imgages" si c'est bien le nom du dossier
-        alt="Logo Ici Volets Roulants"
-        width={50}  // Ajustez la largeur souhaitée (en pixels)
-        height={50} // Ajustez la hauteur souhaitée (en pixels)
-        className="h-10 w-auto md:h-12" // Taille adaptative en CSS (hauteur fixe, largeur auto)
+        alt="Logo réparation volets roulants"
+        width={70}  // Ajustez la largeur souhaitée (en pixels)
+        height={70} // Ajustez la hauteur souhaitée (en pixels)
+        className="h-12 w-auto md:h-12" // Taille adaptative en CSS (hauteur fixe, largeur auto)
         priority // Charge l'image en priorité car c'est le logo
       />
     </div>
 
     {/* Conteneur du texte (flex-col pour garder l'empilement) */}
     <div className="flex flex-col">
-      <span className="text-2xl font-bold md:text-3xl tracking-widest text-black">
+      <span className="text-2xl font-bold md:text-4xl tracking-widest text-black">
         icivoletsroulants
       </span>
       
-      <span className="hidden md:block text-sm font-medium -mt-1 tracking-[0.09em] text-gray-700">
-        réseau national répatateurs de volets
+      <span className="hidden md:block text-sm font-medium -mt-1 tracking-[0.07em] text-gray-700">
+        réseau national répatateurs de volets roulants
       </span>
     </div>
   </div>
