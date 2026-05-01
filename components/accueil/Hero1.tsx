a1f324d1173f099880ff7d9b2c50145ea686fedb
Alami ERHAIDI <alamierhaidi@iMac-de-Alami-2.local>
2026-05-01 08:36:37 +0200

    first commit

--- components/accueil/Hero1.tsx
+++ components/accueil/Hero1.tsx
@@ -39,7 +39,7 @@ export default function Hero1() {
   Vous souhaitez réparer vos volets roulants ?
 </h1>
 
-            <p className="mt-6 text-black text-lg md:text-xl leading-relaxed ">
+            <p className="mt-6 text-black text-base md:text-xl leading-relaxed ">
               <strong>Icivoletsroulants</strong> est spécialisé dans la <strong>réparation de volets roulants, stores, portes de garage et moteurs de portail</strong> dans toute la France.
             </p>
 
